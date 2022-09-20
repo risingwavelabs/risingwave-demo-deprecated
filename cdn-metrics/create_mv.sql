8c5e236cc5be168cbbc6bd74de8191c6de6a1bde
Tao Wu <wutao@singularity-data.com>
2022-09-20 23:29:35 +0800

    add test cdn-metrics

--- cdn-metrics/create_mv.sql
+++ cdn-metrics/create_mv.sql
@@ -1,30 +1,3 @@
-CREATE SOURCE nics_metrics (
-    device_id VARCHAR,
-    metric_name VARCHAR,
-    aggregation VARCHAR,
-    nic_name VARCHAR,
-    report_time TIMESTAMP,
-    bandwidth DOUBLE PRECISION,
-    metric_value DOUBLE PRECISION
-) WITH (
-    connector = 'kafka',
-    kafka.topic = 'nics_metrics',
-    kafka.brokers = 'message_queue:29092',
-    kafka.scan.startup.mode = 'earliest'
-) ROW FORMAT JSON;
-
-CREATE SOURCE tcp_metrics (
-    device_id VARCHAR,
-    metric_name VARCHAR,
-    report_time TIMESTAMP,
-    metric_value DOUBLE PRECISION
-) WITH (
-    connector = 'kafka',
-    kafka.topic = 'nics_metrics',
-    kafka.brokers = 'message_queue:29092',
-    kafka.scan.startup.mode = 'earliest'
-) ROW FORMAT JSON;
-
 CREATE MATERIALIZED VIEW high_util_tcp_metrics AS
 SELECT
     tcp.device_id AS device_id,
