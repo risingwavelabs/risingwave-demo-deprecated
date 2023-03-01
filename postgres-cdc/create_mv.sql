be463964eb2286d9c14c6a2f26c4c0ad40189429
Tao Wu <wutao@singularity-data.com>
2023-03-01 15:52:34 +0800

    fix

--- postgres-cdc/create_mv.sql
+++ postgres-cdc/create_mv.sql
@@ -11,22 +11,9 @@ CREATE MATERIALIZED VIEW nexmark_q8 AS
 SELECT
     P.id,
     P.name,
-    P.starttime
+    S.starttime
 FROM
-    (
-        SELECT
-            id,
-            name,
-            window_start AS starttime,
-            window_end AS endtime
-        FROM
-            TUMBLE(person, date_time, INTERVAL '10' SECOND)
-        GROUP BY
-            id,
-            name,
-            window_start,
-            window_end
-    ) P
+    person as P
     JOIN (
         SELECT
             seller,
@@ -38,6 +25,4 @@ FROM
             seller,
             window_start,
             window_end
-    ) A ON P.id = A.seller
-    AND P.starttime = A.starttime
-    AND P.endtime = A.endtime;
+    ) A ON P.id = A.seller;
