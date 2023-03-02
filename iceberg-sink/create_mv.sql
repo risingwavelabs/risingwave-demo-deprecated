fa6e060f29cec524607d955197f146399e346218
Tao Wu <wutao@singularity-data.com>
2023-03-02 17:18:55 +0800

    fix

--- iceberg-sink/create_mv.sql
+++ iceberg-sink/create_mv.sql
@@ -2,7 +2,7 @@ CREATE MATERIALIZED VIEW seller_auctions AS
 SELECT
     P.id as seller_id,
     P.name as seller_name,
-    A.date_time as auction_time,
+    A.date_time::varchar as auction_time,
     A.item_name as auction_item_name,
     A.category as auction_category,
     A.id as auction_id
--- iceberg-sink/spark-script/create-table.sql
+++ iceberg-sink/spark-script/create-table.sql
@@ -6,5 +6,6 @@ CREATE TABLE demo.demo_db.seller_auctions (
   auction_time timestamp with time zone,
   auction_item_name string,
   auction_category int,
-  auction_id bigint
+  auction_id bigint,
+  _row_id bigint
 ) TBLPROPERTIES ('format-version' = '2');
